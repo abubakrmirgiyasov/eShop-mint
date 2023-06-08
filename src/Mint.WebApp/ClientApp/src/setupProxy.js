bde6ed90b2f2bb273cb534de298ffdf822532dd3
Abubakr <abubakrmirgiyasov@gmail.com>
2023-06-08 15:52:09 +0700

    Fix

--- src/Mint.WebApp/ClientApp/src/setupProxy.js
+++ src/Mint.WebApp/ClientApp/src/setupProxy.js
@@ -1,6 +1,8 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const { env } = require('process');
 
+// const target =  'https://localhost:44411';
+
 const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
   env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:62257';
 
